b6cd3147faf1155574c92db739dfa3ff7d5743bc
GeonHo Song <songgun1234@gmail.com>
2025-02-05 19:19:15 +0900

    feat : netlify

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    //Provider 전역 설정
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>
-          <Navbar />
-          <Outlet />
-        </AuthContextProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    //Provider 전역 설정 <B basename={process.env.PUBLIC_URL}>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Navbar />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
