a27215edabc1499815b7427404ed727821874a84
GeonHo Song <songgun1234@gmail.com>
2025-02-05 18:51:00 +0900

    feat : netlify 제발...

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     //Provider 전역 설정
-    <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <Navbar />
-        <Outlet />
-      </AuthContextProvider>
-    </QueryClientProvider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <Navbar />
+          <Outlet />
+        </AuthContextProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
